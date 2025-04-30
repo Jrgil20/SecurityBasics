3c7ea4f426d4fadbe8a6a9c54211fd2e04b05259
Jesus Rodolfo Gil farias <jrgil.20@est.ucab.edu.ve>
2025-04-30 02:56:04 +0000

    Revert "chore: update dependencies in package.json"
    This reverts commit 7a1a3d76049e74fe093566d37091b9d3121c8a57.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { LanguageProvider } from "@/components/language-provider"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -26,7 +25,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <LanguageProvider>{children}</LanguageProvider>
-          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
